fd9f616be27feabe1dea8d1540bad7631d36fd38
jinltan9-maker <jinltan9@gmail.com>
2026-05-18 14:54:30 +1000

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,1 @@
-from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder='.')
-
-@app.route('/')
-def index():
-    return send_from_directory('.', 'taskpane.html')
-
-@app.route('/<path:path>')
-def serve_file(path):
-    return send_from_directory('.', path)
-
-if __name__ == '__main__':
-    app.run(ssl_context='adhoc', port=3000)
